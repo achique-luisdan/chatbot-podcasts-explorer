91d4fbe20d14bcc67210b1dc7d347a3cfbe1c59d
achique-luisdan <achique.luisdan@gmail.com>
2021-12-06 16:20:41 -0400

    Preparación para configurar linter ESLint

--- tests/list-podcasts.spec.ts
+++ tests/list-podcasts.spec.ts
@@ -5,9 +5,9 @@ import { test, expect } from '@playwright/test';
 let URL_ROOT = 'https://podcasts.google.com/'
 
 test('Listar Podcasts', async ({ page }) => {
-  await page.goto(URL_ROOT);
-  await page.fill('[aria-label="Buscar podcasts"]', 'Desarrollo Web');
-  await page.click('[aria-label="Búsqueda"]');  
-  await page.screenshot({path: 'screenshot.png'});
+  await page.goto(URL_ROOT)
+  await page.fill('[aria-label="Buscar podcasts"]', 'Desarrollo Web')
+  await page.click('[aria-label="Búsqueda"]') 
+  //await page.screenshot({path: 'screenshot.png'});
   await page.pause();
 });
